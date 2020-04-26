ee9c518cca6ff2b49657175382001f00607a22fd
ShihabZzz <Shihab.riadn@gmail.com>
2020-04-26 18:52:42 +0530

    sdm845-common: Build LDAC decoder library
    Change-Id: I60b10e0adbc606a49dedd1c272f3b1e84f44e625
    Signed-off-by: Cruzer1 <preetamswain143@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -100,6 +100,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
     libldacBT_enc \
+    libldacBT_dec \
     libldacBT_abr \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
