5d90dd30a204d6ce57078cd5abdc5b05bce536d6
Cruzer1 <preetamswain143@gmail.com>
2022-11-26 02:42:29 +0000

    sdm845-common: Move Camera shim to device tree
    Signed-off-by: Cruzer1 <preetamswain143@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -107,7 +107,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     libdng_sdk.vendor \
-    libpiex_shim \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Charger
